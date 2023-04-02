e151cf008dffddf57775798c892d73d68a91b223
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-01 18:02:13 -0700

    left add

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,14 +14,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->left == NULL)
 		parent->left = new_node;
